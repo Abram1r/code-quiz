8c5f4425c815af17233d3f3303311c5e9b4e198f
Dodge <r.abram93@gmail.com>
2022-05-28 14:18:10 -0500

    added questions to js

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+// questions, choices, and answers
+
+var questions = [
+{
+    question: "Commonly used data types do NOT include:",
+    choices: ["strings", "booleans", "numbers", "alerts"],
+    answer: "alerts"
+},
+{
+   question: "How do you write a comment in JavaScript?",
+   choices: ["<!-- This is a comment -->", "// This is a comment","'This is a comment'", "/* This is a comment */"],
+   answer: "// This is a comment" 
+},
+{
+   question: "Arrays in JavaScript can be used to store ____.",
+   choices: ["booleans", "other arrays", "numbers and strings", "all of the above"],
+   answer: "all of the above"
+},
+{
+    question: "The condition of an if/else statement is enclosed with ____.",
+    choices: ["parenthesis", "quotes", "curly brackets", "square brackets"],
+    answer: "curly brackets"
+},
+{
+    question: "String values must be enclosed within ___ when being assigned to variables.",
+    choices: ["quotes", "commas", "parenthesis", "square brackets"],
+    answer: "quotes"
+}
+];
